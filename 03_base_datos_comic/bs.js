65380d2fabbc2bb210040ef63b5463d1eca2b6d5
Sebastián Acevedo González <sebsag10@gmail.com>
2025-11-05 17:24:37 -0500

    base de datos
    base de datos

---
+++ 03_base_datos_comic/bs.js
@@ -0,0 +1,47 @@
+let comic={
+  "nombreComic": "Arkana",
+  "numeroCapitulos": "3",
+  "genero": "bibiografico",
+  "sinopsis":"...",
+  "autores":["sebastian","ostin"],
+
+  "year":2025,
+  "descripcion":"informacion adicional",
+  "capitulos": [
+    {
+      "id": 1,
+      "nombre": "El origen",
+      "personajes": "...", 
+       "portada":"url de la portada",
+"descripcion":"descripcion del capitulo 1"
+    },
+    {
+      "id": 2,
+      "nombre": "La batalla",
+      "personajes": "...", 
+       "portada":"url de la portada",
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
